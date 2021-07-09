1c5aec025c588b7b59a4c95c548d0d8ccdbc6d1a
kaizerxcx <johnkaizer.tabanag@cit.edu>
2021-07-09 21:00:24 +0800

    SpekPuzz

---
+++ Assets/Scripts/TouchRotate.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnMouseDown()
+    {
+        if (!GameControl.youWin)
+            transform.Rotate(0f, 0f, 90f);
+    }
+}
