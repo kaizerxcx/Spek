224d5a57e2046e8eb0fab0fe8db891862840a92f
kaizerxcx <johnkaizer.tabanag@cit.edu>
2021-07-09 23:51:49 +0800

    Spek Main Menu

--- Assets/Scripts/NameThatSpeciesController.cs
+++ Assets/Scripts/NameThatSpeciesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEngine.SceneManagement;
 public class NameThatSpeciesController : MonoBehaviour
 {
     public Button correct;
